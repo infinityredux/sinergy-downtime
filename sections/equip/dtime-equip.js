c9f12f8d0882c6db909051a4b1a6c0266107ad96
Nick <kedomir@gmail.com>
2016-08-21 14:56:33 +0100

    Changed equipment to a component (instead of directive + controller).

--- sections/equip/dtime-equip.js
+++ sections/equip/dtime-equip.js
@@ -10,67 +10,63 @@ mod = angular.module('sin.dtime.equip', [
   'sin.lib.persist'
   ]);
 
-mod.directive('dtEquip', function() {
-  return {
-    restrict: 'E',
-    scope: {
-      dtime: '='
-    },
+mod.component('dtEquip', {
+    controller: EquipController,
+    controllerAs: 'ctrl',
     templateUrl: 'sections/equip/template-equip.html'
-  };
 });
 
-mod.controller('EquipController', function($scope, dtime, assets, equip, lifestyle, skills, persist) {
-  var ctrl = this;
-  $scope.dtime = dtime;
-  $scope.assetJob = assets.getJob();
-  $scope.equip = equip;
-  $scope.skills = skills;
+function EquipController($scope, dtime, assets, equip, lifestyle, skills, persist) {
+    var ctrl = this;
+    $scope.dtime = dtime;
+    $scope.assetJob = assets.getJob();
+    $scope.equip = equip;
+    $scope.skills = skills;
 
-  ctrl.state = {};
-  ctrl.data = {
-    types: {
-      item:     {val: "item", desc: "Item", help:"A piece of equipment that doesn't fit in the other types. This includes guns, armour, tools, etc."},
-      cyber:    {val: "cyber", desc: "Cyberware", help:"An item of cyberware installed in one or more locations (slots) in the human body."},
-      bio:      {val: "bio", desc: "Bioware", help:"An item of bioware installed anywhere in the body."},
-      deck:     {val: "deck", desc: "Cyberdeck", help:"Any variety of cyberdeck or server.  For a cyberware deck you will need to add the item in both this section and cyberware."},
-      software: {val: "software", desc: "Software", help: "Software that runs on a cyberdeck."},
-      contact:  {val: "contact", desc: "Contact", help: "A contact or friend that can assist you in downtime"}
-    }
-  };
-  
-  var defaultState = function() {
-    ctrl.state.type = '';
-  };
-  
-  defaultState();
-  
-  persist.registerLoad(function() {
-    ctrl.state = persist.doLoad('sin.dtime.equip:state', ctrl.state);
-  });
+    ctrl.state = {};
+    ctrl.data = {
+        types: {
+            item:     {val: "item", desc: "Item", help:"A piece of equipment that doesn't fit in the other types. This includes guns, armour, tools, etc."},
+            cyber:    {val: "cyber", desc: "Cyberware", help:"An item of cyberware installed in one or more locations (slots) in the human body."},
+            bio:      {val: "bio", desc: "Bioware", help:"An item of bioware installed anywhere in the body."},
+            deck:     {val: "deck", desc: "Cyberdeck", help:"Any variety of cyberdeck or server.  For a cyberware deck you will need to add the item in both this section and cyberware."},
+            software: {val: "software", desc: "Software", help: "Software that runs on a cyberdeck."},
+            contact:  {val: "contact", desc: "Contact", help: "A contact or friend that can assist you in downtime"}
+        }
+    };
 
-  persist.registerSave(function() {
-    persist.doSave('sin.dtime.equip:state', ctrl.state);
-  });
+    var defaultState = function() {
+        ctrl.state.type = '';
+    };
 
-  persist.registerWipe(function() {
     defaultState();
-  });
 
-  ctrl.optChange = function () {
-    
-  };
-  
-  ctrl.newClick = function() {
-    if (ctrl.state.type === '')
-      return false;
-      
-    var item = {};
-    item.name = 'Test item';
-    item.type = ctrl.state.type;
-    
-    var details = {};
-    
-    equip.addEquip(item, details);
-  };
-});
+    persist.registerLoad(function() {
+        ctrl.state = persist.doLoad('sin.dtime.equip:state', ctrl.state);
+    });
+
+    persist.registerSave(function() {
+        persist.doSave('sin.dtime.equip:state', ctrl.state);
+    });
+
+    persist.registerWipe(function() {
+        defaultState();
+    });
+
+    ctrl.optChange = function () {
+
+    };
+
+    ctrl.newClick = function() {
+        if (ctrl.state.type === '')
+            return false;
+
+        var item = {};
+        item.name = 'Test item';
+        item.type = ctrl.state.type;
+
+        var details = {};
+
+        equip.addEquip(item, details);
+    };
+}
