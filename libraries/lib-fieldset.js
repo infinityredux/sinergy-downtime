7bee9f1e9d0d9a8baed7bbd4c2888468658b78a3
Nick <kedomir@gmail.com>
2016-09-06 01:15:03 +0100

    Fixing whitespace, etc.

--- libraries/lib-fieldset.js
+++ libraries/lib-fieldset.js
@@ -1,37 +1,35 @@
-// Code goes here
-
 mod = angular.module('sin.lib.fieldset', []);
 
 mod.directive('sinFieldset', function() {
-  return {
-    restrict: 'E',
-    transclude: true,
-    scope: {
-      legend: '@',
-      collapsed: '@'
-    },
-    templateUrl: 'libraries/template-fieldset.html',
-    link: function(scope, element, attrs) {
-      scope.test = attrs;
-      scope.hide = (attrs.collapsed == "true");
-      scope.fieldsetLegend = attrs.legend;
-      
-      if(scope.hide) {
-        element.children().addClass("fieldset-closed");
-      } else {
-        element.children().addClass("fieldset-open");
-      }
-      
-      scope.toggleCollapse = function() {
-        scope.hide = !scope.hide;
-        if(scope.hide) {
-          element.children().addClass("fieldset-closed");
-          element.children().removeClass("fieldset-open");
-        } else {
-          element.children().addClass("fieldset-open");
-          element.children().removeClass("fieldset-closed");
+    return {
+        restrict: 'E',
+        transclude: true,
+        scope: {
+            legend: '@',
+            collapsed: '@'
+        },
+        templateUrl: 'libraries/template-fieldset.html',
+        link: function(scope, element, attrs) {
+            scope.test = attrs;
+            scope.hide = (attrs.collapsed == "true");
+            scope.fieldsetLegend = attrs.legend;
+
+            if(scope.hide) {
+                element.children().addClass("fieldset-closed");
+            } else {
+                element.children().addClass("fieldset-open");
+            }
+
+            scope.toggleCollapse = function() {
+                scope.hide = !scope.hide;
+                if(scope.hide) {
+                    element.children().addClass("fieldset-closed");
+                    element.children().removeClass("fieldset-open");
+                } else {
+                    element.children().addClass("fieldset-open");
+                    element.children().removeClass("fieldset-closed");
+                }
+            };
         }
-      };
-    }
-  };
+    };
 });
--- libraries/lib-tabbed-panes.js
+++ libraries/lib-tabbed-panes.js
@@ -1,73 +1,71 @@
-// Code goes here
-
 var mod = angular.module("sin.lib.panes", []);
 
 mod.directive('sinTabbedPanes', function() {
-  return {
-    restrict: 'E',
-    scope: true,
-    bindToController: {
-      initial: '@'
-    },
-    transclude: true,
-    template: '<span ng-transclude></span>',
-    controllerAs: 'tabCtrl',
-    controller: function() {
-      this.current = this.initial;
-      this.isCurrent = function(test) {
-        return (test == this.current);
-      };
-      this.setCurrent = function(change) {
-        this.current = change;
-      };
-    }
-  };
+    return {
+        restrict: 'E',
+        scope: true,
+        bindToController: {
+            initial: '@'
+        },
+        transclude: true,
+        template: '<span ng-transclude></span>',
+        controllerAs: 'tabCtrl',
+        controller: function() {
+            this.current = this.initial;
+            this.isCurrent = function(test) {
+                return (test == this.current);
+            };
+            this.setCurrent = function(change) {
+                this.current = change;
+            };
+        }
+    };
 });
 
 mod.directive('sinPaneLink', function() {
-  return {
-    require: '^^sinTabbedPanes',
-    restrict: 'E',
-    scope: {
-      tab: '@',
-      action: '&'
-    },
-    transclude: true,
-    template: '<span ng-transclude ng-click="doClick()" ng-class="getClass()"></span>',
-    link: function(scope, elem, attrs, tabCtrl) {
-      scope.doClick = function() {
-        tabCtrl.setCurrent(attrs.tab);
-        if (attrs.action !== undefined) {
-          //attrs.action();
-          //alert(attrs.action);
-          scope.action();
+    return {
+        require: '^^sinTabbedPanes',
+        restrict: 'E',
+        scope: {
+            tab: '@',
+            action: '&'
+        },
+        transclude: true,
+        template: '<span ng-transclude ng-click="doClick()" ng-class="getClass()"></span>',
+        link: function(scope, elem, attrs, tabCtrl) {
+            scope.doClick = function() {
+                tabCtrl.setCurrent(attrs.tab);
+                if (attrs.action !== undefined) {
+                    //attrs.action();
+                    //alert(attrs.action);
+                    scope.action();
+                }
+            };
+            scope.getClass = function() {
+                if (tabCtrl.isCurrent(attrs.tab)) {
+                    return 'pane-link-active';
+                }
+                else {
+                    return 'pane-link-passive';
+                }
+            };
         }
-      };
-      scope.getClass = function() {
-        if (tabCtrl.isCurrent(attrs.tab)) {
-          return 'pane-link-active';
-        }
-        else {
-          return 'pane-link-passive';
-        }
-      };
-    }
-  };
+    };
 });
 
 mod.directive('sinPane', function() {
-  return {
-    require: '^^sinTabbedPanes',
-    restrict: 'E',
-    scope: {
-      tab: '@'
-    },
-    transclude: true,
-    template: '<span ng-transclude ng-show="canShow()"></span>',
-    link: function(scope, elem, attrs, tabCtrl) {
-      scope.canShow = function() {
-        return tabCtrl.isCurrent(attrs.tab) || tabCtrl.isCurrent('all');
-      };
-    }
-  };
+    return {
+        require: '^^sinTabbedPanes',
+        restrict: 'E',
+        scope: {
+            tab: '@'
+        },
+        transclude: true,
+        template: '<span ng-transclude ng-show="canShow()"></span>',
+        link: function(scope, elem, attrs, tabCtrl) {
+            scope.canShow = function() {
+                return tabCtrl.isCurrent(attrs.tab) || tabCtrl.isCurrent('all');
+            };
+        }
+    };
 });
